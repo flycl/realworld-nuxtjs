a01d35d9063f90dba19e63d1af6109370cf57e7e
Kevin <kevin.chen@echosens.com>
2021-08-30 15:42:34 +0800

    fix: chang main page title

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,7 +4,7 @@
     <div class="banner">
       <div class="container">
         <h1 class="logo-font">ETH.DESIGN</h1>
-        <p>A place to share your knowledge.</p>
+        <p>www.eth.design</p>
       </div>
     </div>
 
